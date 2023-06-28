009ffa7990e6798e1d7f0313cf41a97650cead7d
Prashant Singh <prashant3jan@gmail.com>
2023-06-28 18:45:10 +0530

    corrected various app defects due to conversion to MongoDB

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
@@ -13,7 +14,7 @@ import java.math.BigDecimal;
 @Setter
 public class Ingredient {
 
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
     @DBRef
